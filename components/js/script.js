359ee16e32ee52d3e40c1e8cf3b1f2eb761e8592
Natalia Loskutova <natalie.loskutova@gmail.com>
2015-08-14 18:31:44 +0300

    2 pages ( main & inner )

--- components/js/script.js
+++ components/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         var el = $(this).attr('href');
         if ($(window).width() > 991) {
         $('body').animate({
-            scrollTop: $(el).offset().top-209}, 500);
+            scrollTop: $(el).offset().top-159}, 500);
         return false; 
     } else {
     	 $('body').animate({
@@ -28,10 +28,25 @@ $(document).ready(function(){
   		 adaptiveHeight: true
 		});
   	// модальное окно обратной связи. пока повешено на блок заказать звонок
+    // модальное окно без текстового поля
   	 $('.modal-call').fancybox();
      $('.modal-call').click(function(e){
      	e.preventDefault();
      });
+     // модалка с текстерией
+    $('.modal-call2').fancybox({
+      autoHeight: true,
+      minHeight: 560,
+      autoCenter: true
+     });
+     $('.modal-call2').click(function(e){
+      e.preventDefault();
+     });
+     // модалка выведения отправки или ошибки отправки
+      $('.feedback_button').fancybox({
+    maxHeight: 30
+   });
+
    //отработка табов в рекмендуемых
   	$('.recommend__content-item.cinema').show();
   	$('.recommend-tabs__item.cinema').css('border-bottom-color', '#d4261b');
@@ -89,4 +104,93 @@ $(document).ready(function(){
        		}
      	});
    	});
+
+
+ // lightbox-form
+    function sentClientInfoToMail() {
+
+    var name = $("#modalForm").find('[name="Feedback[name]"]').val();
+    var telephone = $("#modalForm").find('[name="Feedback[telephone]"]').val();
+    var mail = $("#modalForm").find('[name="Feedback[email]"]').val();
+    var infotext = $("#feedback_info").find('.throw_error');
+    if (name == "" || telephone == "") {
+        infotext.html('Заполните все поля...');
+        infotext.css('color', 'red');
+        $( '[href="#feedback_info"]' ).click();
+    } else {
+  $.ajax({
+    type: "POST",
+    url: "/ajax/messages/feedback",
+    data: {
+        'Feedback[name]': name,
+        'Feedback[telephone]': telephone,
+        'Feedback[email]': mail
+        },
+    success: function(data) {
+            infotext.html('Заявка отправлена!');
+            infotext.css('color', '#f99');
+            $( '[href="#feedback_info"]' ).click();
+            $("input, textarea").val("");
+          },
+        error:  function() {
+        infotext.html('Ошибка отправки...');
+          infotext.css('color', 'red');
+          $( '[href="#feedback_info"]' ).click();
+        }
+    
+    });
+  }
+};
+//  select form
+
+    function sentClientMsgToMail() {
+
+    var name = $("#modalForm2").find('[name="product[name]"]').val();
+    var telephone = $("#modalForm2").find('[name="product[telephone]"]').val();
+    var mail = $("#modalForm2").find('[name="product[email]"]').val();
+    var msg = $("#modalForm2").find('[name="product[text]"]').val();
+    var text ="Почта: "+mail+", Сообщение: "+msg;
+    var infotext = $("#feedback_info").find('.throw_error');
+    if (name == "" || telephone == "") {
+        infotext.html('Заполните все поля...');
+        infotext.css('color', 'red');
+        $( '[href="#feedback_info"]' ).click();
+    } else {
+  $.ajax({
+    type: "POST",
+    url: "/ajax/messages/feedback",
+      data: {
+          'Feedback[name]': name,
+          'Feedback[telephone]': telephone,
+          'Feedback[email]': mail,
+          'Feedback[text]': text
+        },
+    success: function(data) {
+            infotext.html('Заявка отправлена!');
+            infotext.css('color', '#f99');
+            $( '[href="#feedback_info"]' ).click();
+            $("input, textarea").val("");
+          },
+        error:  function() {
+        infotext.html('Ошибка отправки...');
+          infotext.css('color', 'red');
+          $( '[href="#feedback_info"]' ).click();
+        }
+    });
+  }
+};
+
+// вызов обработчиков
+ $("#modalForm .submit").click(function(event) {
+  event.preventDefault();
+      
+        sentClientInfoToMail();
+    });
+
+ $("#modalForm2 .submit").click(function(event) {
+  event.preventDefault();
+      
+        sentClientMsgToMail();
+    });
+
 });
